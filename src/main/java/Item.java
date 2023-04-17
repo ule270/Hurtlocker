d7c5bb956e550a9fa19ac375036f45691eb08582
ule270 <u.le270@gmail.com>
2023-04-17 11:05:58 -0400

    item class made with getters/setters

---
+++ src/main/java/Item.java
@@ -0,0 +1,44 @@
+public class Item {
+    private String name;
+    private String price;
+    private String type;
+    private String expiration;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getPrice(){
+        return price;
+    }
+
+    public void setPrice(){
+        this.price = price;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public void setType(){
+        this.type = type;
+    }
+
+    public String getExpiration(){
+        return expiration;
+    }
+
+    public void setExpiration(){
+        this.expiration = expiration;
+    }
+
+    public Item(String name, String price, String type, String expiration){
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.expiration = expiration;
+    }
+}
